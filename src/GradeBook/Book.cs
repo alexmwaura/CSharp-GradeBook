d96eefe50431ea3d9482150c15a93d6586371668
alexmwaura <alexmwaura43@gmail.com>
2020-12-04 01:03:26 +0300

    add constructor

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
 namespace GradeBook
 {
-    public class Book
+    class Book
     {
-        
+        public Book()
+        {
+            grades = new List<double>();
+        }
+        List<double> grades;
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
     }
 }
