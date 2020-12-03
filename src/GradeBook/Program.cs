84221fcf11b3cd7cee4334ea36fd690c14f2473f
alexmwaura <alexmwaura43@gmail.com>
2020-12-04 01:03:43 +0300

    initialize method

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,7 +5,10 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            var book = new Book();
+            book.AddGrade(89.1);
+            
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
             
