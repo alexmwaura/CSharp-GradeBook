2c3c1a852e15bf6833be7edca76aa09800da13f6
alexmwaura <alexmwaura43@gmail.com>
2020-12-04 03:18:52 +0300

    refactor

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,28 +6,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {   
-            var book = new Book();
+            var book = new Book("Alex's  Grade book");
             book.AddGrade(89.1);
-            
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
-            
-            var result = 0.0;
-            foreach (double number in grades)
-            {
-                result += number;
-            }
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N3}");
-
-            if (args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Hello");
-            }
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();
         }
     }
 }
