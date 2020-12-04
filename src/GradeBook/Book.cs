591bc78270ff8468d86a8098bf0db33864d9ace6
alexmwaura <alexmwaura43@gmail.com>
2020-12-04 03:18:40 +0300

    add method

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,17 +1,34 @@
+using System;
 using System.Collections.Generic;
 namespace GradeBook
 {
     class Book
     {
-        public Book()
+        private List<double> grades;
+        private string name;
+        public Book(string name)
         {
             grades = new List<double>();
+            this.name = name;
         }
-        List<double> grades;
 
         public void AddGrade(double grade)
         {
             grades.Add(grade);
+        }
+        public void ShowStatistics(){
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach(double number in grades){
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number; 
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
         }
 
     }
